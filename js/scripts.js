9de4a28a1c7e845151ad205d8afe5c4742d2ead1
Suresh Thapa <iamtechnicalsuresh@gmail.com>
2022-02-07 16:26:39 +0530

    dropdown menu added

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,19 @@ const humburger = document.querySelector(".humburger");
 const navlinks = document.querySelector(".navlinks");
 const menuBars = document.querySelector(".fa-bars");
 
+const dropdown = document.querySelector(".dropdown");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+const footerDate = document.querySelector(".date");
+
 humburger.addEventListener("click", (e) => {
-  console.log("clicked");
   navlinks.classList.toggle("navlinks-open");
   menuBars.classList.toggle("fa-times");
 });
+
+dropdown.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("dropdown-menu-open");
+});
+
+const currentYear = new Date().getFullYear();
+footerDate.innerHTML = currentYear;
